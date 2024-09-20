41b89e74c02a15b5f387f069264d3a4a4f7c067e
sotiristiga <99202968+sotiristiga@users.noreply.github.com>
2024-09-20 16:43:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,34 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+
+
+st.set_page_config(layout='wide',page_title="Tiganitas Sotiris Dashboard Portofolio",page_icon="")
+
+st.title("My dashboard portofolios📈📊")
+st.write("#### ")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "### - 🏀 Euroleague Stats:"
 )
+
+st.write("##### * RShinyapp (R software) ~ https://tiganitassot.shinyapps.io/Euroleague/ ")
+st.write("It takes approximately 2 minutes to load all stats")
+
+st.write("##### * Streamlit app (Python software) ~ https://euroleaguebasketball.streamlit.app/ ")
+st.write(" It loads all stats quicker than RShiny")
+st.write("#### ")
+st.write(
+    "### - 💡 Belyse Lights: Analyze the orders of the eshop")
+st.write("##### https://belyselights.streamlit.app/ ")
+st.write("#### ")
+
+st.write(
+    "### - 📋 Insurance Broker portofolio: Analyze the insurance policies and the development of the company")
+st.write("##### https://stportofoliomanage.streamlit.app/ ")
+
+st.write("#### ")
+st.write(
+    "### - 📋 Insurance Broker portofolio: Analyze the insurance policies and the development of the company")
+st.write("##### https://insurancebrokerftportofolio.streamlit.app/ ")
+
+st.write("#### ")
+st.write('#### - For more information contact to tiganitassotiris@gmail.com')
